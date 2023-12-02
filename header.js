f67d561659de1045cff7f534f6cf327b290cd262
anandkrishnan108 <93841468+anandkrishnan108@users.noreply.github.com>
2023-12-02 14:24:33 -0600

    Hide soccer

--- header.js
+++ header.js
@@ -33,7 +33,7 @@ class Header extends HTMLElement {
                   <i class="fa fa-caret-down"></i>
               </button>
               <div class="dropdown-content">
-                  <a href="soccer.html">Playing</a>
+                  <!--<a href="soccer.html">Playing</a>-->
                   <a href="referee.html">Refereeing</a>
                   <a href="topsoccer.html">Volunteering</a>
                   </div>
@@ -50,8 +50,6 @@ class Header extends HTMLElement {
               <div class="dropdown-content">
                   <a href="mailto:akrishx@gmail.com">Email: akrishx@gmail.com</a>
                   <a href="https://www.linkedin.com/in/anand-krishnan-0a62621ab/" target="_blank">LinkedIn</a>
-                  <a href="https://www.instagram.com/akrishx/" target="_blank">Insta: akrishx</a>
-                  <a href="#">Snap: akrishx5</a>
               </div>
           </div>
         </div>  
